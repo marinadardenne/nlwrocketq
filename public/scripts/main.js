47d15436fee13367574411d8069f8cb19080f4ea
Marina Dardenne <marina.mdardenne@gmail.com>
2021-08-19 21:48:17 -0300

    Configuração do banco de dados, rotas e outros ajustes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -34,7 +34,7 @@ function handleClick(event, check = true) {
     const questionId = event.target.dataset.id
 
     const form = document.querySelector(".modal form")
-    form.setAttribute("action", `/room/${roomId}/${questionId}/:${slug}`)
+    form.setAttribute("action", `/question/${roomId}/${questionId}/:${slug}`)
 
     //estrutura de ternários
     modalTitle.innerHTML = check ? "Marcar como lida" : "Excluir esta pergunta"
